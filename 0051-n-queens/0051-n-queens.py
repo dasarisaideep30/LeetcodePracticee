ff6c8149accc5e66524c7fd11f3601619d92ba2a
dasarisaideep30 <saideepdasari30@gmail.com>
2026-03-01 19:19:00 +0530

    Time: 11 ms (87.54%), Space: 12.6 MB (53.79%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def solveNQueens(self, n):
+        """
+        :type n: int
+        :rtype: List[List[str]]
+        """
+        res=[]
+        def bt(r,c,d1,d2,path):
+            if r==n:
+                res.append(["."*i+"Q"+"."*(n-i-1) for i in path])
+                return res
+            for i in range(n):
+                if i not in c and r-i not in d1 and r+i not in d2:
+                    bt(r+1,c|{i},d1|{r-i},d2|{r+i},path+[i])
+        bt(0,set(),set(),set(),[])
+        return res
