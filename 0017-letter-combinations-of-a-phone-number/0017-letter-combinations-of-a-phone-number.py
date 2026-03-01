72e44025e22a6e81a7b587e60a21d99b24a0e333
dasarisaideep30 <saideepdasari30@gmail.com>
2026-03-01 15:55:09 +0530

    Time: 2 ms (8.8%), Space: 12.3 MB (99.64%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,9 +1,13 @@
 class Solution(object):
-    def letterCombinations(self,digits):
+    def letterCombinations(self, digits):
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
         if not digits:
             return []
-        phonemap = {'2':'abc','3':'def','4':'ghi','5':'jkl','6':'mno','7':'pqrs','8':'tuv','9':'wxyz'}
-        result = []
+        phonemap={'2':'abc','3':'def','4':'ghi','5':'jkl','6':'mno','7':'pqrs','8':'tuv','9':'wxyz'}
+        result=[]
         def backtrack(index,path):
             if index==len(digits):
                 result.append("".join(path))
@@ -14,4 +18,4 @@ class Solution(object):
                 backtrack(index+1,path)
                 path.pop()
         backtrack(0,[])
-        return result 
+        return result
