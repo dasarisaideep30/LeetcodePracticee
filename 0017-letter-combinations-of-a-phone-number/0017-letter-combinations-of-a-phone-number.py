848999a8e44560ad76c8d1ee701664b0ecb9e497
dasarisaideep30 <saideepdasari30@gmail.com>
2026-03-02 00:30:33 +0530

    Time: 0 ms (100%), Space: 12.5 MB (56.55%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,9 +1,5 @@
 class Solution(object):
-    def letterCombinations(self, digits):
-        """
-        :type digits: str
-        :rtype: List[str]
-        """
+    def letterCombinations(self,digits):
         if not digits:
             return []
         phonemap={'2':'abc','3':'def','4':'ghi','5':'jkl','6':'mno','7':'pqrs','8':'tuv','9':'wxyz'}
