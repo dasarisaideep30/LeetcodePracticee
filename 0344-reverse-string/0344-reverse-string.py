fa9991aa5f9257d61193a710e829ecc8fbf8861b
dasarisaideep30 <saideepdasari30@gmail.com>
2026-03-02 03:23:48 +0530

    Time: 0 ms (100%), Space: 19.8 MB (96.32%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -4,8 +4,8 @@ class Solution(object):
         :type s: List[str]
         :rtype: None Do not return anything, modify s in-place instead.
         """
-        left = 0
-        right = len(s)-1
+        left=0
+        right=len(s)-1
         while left<right:
             s[left],s[right]=s[right],s[left]
             left+=1
