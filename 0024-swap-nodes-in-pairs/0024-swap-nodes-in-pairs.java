0da734df3bd64db74c3be3db30cdebf7cae42258
dasarisaideep30 <saideepdasari30@gmail.com>
2026-04-01 14:55:49 +0530

    Time: 0 ms (100%), Space: 43 MB (75.79%) - LeetHub

---
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        ListNode dummy = new ListNode(0,head);
+        ListNode cur = dummy;
+        while(cur.next!=null && cur.next.next!=null){
+            ListNode a = cur.next;
+            ListNode b = cur.next.next;
+            a.next=b.next;
+            b.next=a;
+            cur.next=b;
+            cur=a;
+        }
+        return dummy.next;
+    }
+}
