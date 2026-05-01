688b7253252953d9cdfbc886bfe730177c73fb5b
dasarisaideep30 <saideepdasari30@gmail.com>
2026-05-01 14:59:01 +0530

    Time: 0 ms (100%), Space: 47.2 MB (29.8%) - LeetHub

--- 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.java
+++ 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.java
@@ -17,10 +17,14 @@ class Solution {
     public TreeNode insertIntoBST(TreeNode root, int val) {
         if(root==null){
             return new TreeNode(val);
+            // return new Node(val);
         }
         if(val<root.val){
             root.left = insertIntoBST(root.left,val);
         }
+        // if(val<root.data){
+        //     root.left = insertIntoBST(root.left,val);
+        // }
         else{
             root.right = insertIntoBST(root.right,val);
         }
