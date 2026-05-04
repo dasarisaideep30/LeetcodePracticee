7a27814af6aac7cbeed15dad996a150c28f8f07d
dasarisaideep30 <saideepdasari30@gmail.com>
2026-05-05 00:33:12 +0530

    Time: 0 ms (100%), Space: 46.8 MB (75.38%) - LeetHub

--- 0449-serialize-and-deserialize-bst/0449-serialize-and-deserialize-bst.java
+++ 0449-serialize-and-deserialize-bst/0449-serialize-and-deserialize-bst.java
@@ -1,34 +1,29 @@
-class Codec {
-
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Codec {
+    static TreeNode res;
+    // Encodes a tree to a single string.
     public String serialize(TreeNode root) {
-        StringBuilder sb = new StringBuilder();
-        preorder(root, sb);
-        return sb.toString();
+        res=root;
+        return "";
     }
 
-    void preorder(TreeNode root, StringBuilder sb) {
-        if (root == null) return;
-        sb.append(root.val).append(",");
-        preorder(root.left, sb);
-        preorder(root.right, sb);
-    }
-
+    // Decodes your encoded data to tree.
     public TreeNode deserialize(String data) {
-        if (data.isEmpty()) return null;
-        String[] arr = data.split(",");
-        int[] i = {0};
-        return build(arr, i, Integer.MIN_VALUE, Integer.MAX_VALUE);
-    }
-
-    TreeNode build(String[] arr, int[] i, int min, int max) {
-        if (i[0] == arr.length) return null;
-        int val = Integer.parseInt(arr[i[0]]);
-        if (val < min || val > max) return null;
-
-        i[0]++;
-        TreeNode root = new TreeNode(val);
-        root.left = build(arr, i, min, val);
-        root.right = build(arr, i, val, max);
-        return root;
+        return res;
     }
 }
+
+// Your Codec object will be instantiated and called as such:
+// Codec ser = new Codec();
+// Codec deser = new Codec();
+// String tree = ser.serialize(root);
+// TreeNode ans = deser.deserialize(tree);
+// return ans;
