fdad564a6d0a0e944de1cfb705639477ad1a812e
dasarisaideep30 <saideepdasari30@gmail.com>
2026-05-04 16:25:43 +0530

    Time: 3 ms (75.02%), Space: 47.7 MB (10.43%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
@@ -18,7 +18,7 @@ class Solution {
     public boolean findTarget(TreeNode root, int k) {
         return dfs(root,k);
     }
-    boolean dfs(TreeNode root, int k){
+    boolean dfs(TreeNode root,int k){
         // boolean dfs(Node root,int k){}
         if(root==null){
             return false;
